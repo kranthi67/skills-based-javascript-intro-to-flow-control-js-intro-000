54004712b9ee1a8b41689a8716a14cbf90a1d2d4
kranthi kumar <kranthi.8t7@gmail.com>
2019-05-18 03:30:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,3 +3,11 @@ function basicTeenager(age){
     return "your a teenager!";
   }
 }
+function teenager(age){
+  if(age>=13 && age <=19){
+    return "your a teenager!";
+  }
+ else {
+  rturn "You are not a teenager!";
+}
+}
